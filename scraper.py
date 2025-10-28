9428e0478abf27f8bd1e9d3c3e2235c975c798c9
Lizbeth Ramirez <lizbetkr@uci.edu>
2025-10-28 15:00:27 -0700

    Implemented scraper function and extract_next_links

--- scraper.py
+++ scraper.py
@@ -4,44 +4,38 @@ from bs4 import BeautifulSoup
 from lxml import html
 
 DO_NOT_ENTER = set()
-VISITED = set()
+VISITED = set() #CHANGE ALL VISITED TO SEEN 
 COMMON_WORDS = dict()
 SUBDOMAINS = dict()
 LONGEST_PAGE = ('Link', 0)
 
 def scraper(url, resp):
-    # have a do not crawl list
+    global DO_NOT_ENTER
+
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
-    # check if it is NOT in do not crawl list
-        # if it isn't in do not crawl, tokenize and begin analytics
-    # call analytics functions
+    valids = [link for link in links if is_valid(link)]
+
+    if resp.status == 200 and url not in DO_NOT_ENTER:
+        VISITED.add(url)
+        subdomains(url)
+
+        tokens = tokenize(resp)
+        if tokens:
+            confirm_longest_page(url, len(tokens))
+            word_freq(tokens)
+
+
+    if len(VISITED) % 50 == 0:
+        common_words_file()
+        longest_page_file()
+        subdomain_write()
+        unique_urls_write()
+        print(f"[WRITE UPDATE] Processed {len(VISITED)} pages")
+    return valids
 
 def extract_next_links(url, resp):
-    links = []
-    
-    try:
-        if resp.status != 200 or is_valid(url) == False or resp.raw_response is None:
-            return links
-        # normalized_url = normalize_url(url)
-        
-        # Normalize URL
-        # Check/add to unique pages 
-        
-        soup = BeautifulSoup(resp.raw_response.content, 'lxml')
-        # extract hyperlinks
-        for tag in soup.find_all('a', href=True):
-            link = tag['href']
-            absolute_url = urljoin(url, tag['href'])
-            clean_url, _ = urldefrag(absolute_url)
-            links.append(clean_url)
-
-    #Implement stats
-    #dynamically update variables after scraping website
-    #(most common words, unique pages, longest page, subdomains)
-    except Exception as e:
-        print(f"Error extracting links from {url}: {e}")
-    # Implementation required.
+    """ Take a url and it's HTTP response. 
+        Return a set of valid links found on the page, so it can be crawled.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
     # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
@@ -50,26 +44,82 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    """
+    links = set()
     
+    # log urls without 200 response (okay) and return empty set
+    if resp.status != 200 or resp.raw_response is None:
+        DO_NOT_ENTER.add(url)
+        print(f'Skip {url} - HTTP: {resp.status}')
+        return links
+    
+    # check for non-HTML pages (pdf, css, js, etc.)
+    page_type = resp.raw_response.headers.get('Content-Type', '').lower()
+    if 'text/html' not in content_type:
+        DO_NOT_ENTER.add(url)
+        return links
+
+    # Avoid urls with too little/much info
+    if word_count_check(resp):
+        DO_NOT_ENTER.add(url)
+        return links
+    
+    # Already Seen, ignore
+    if url in VISITED: 
+        print('Seen: {url}')
+        return links
+    
+    # Ensure url contains text, not binary 
+    try: 
+        decoded_html = resp.raw_response.content.decode('utf-8', errors='replace')
+    except Exception as e:
+        DO_NOT_ENTER.add(url)
+        return links
+    
+    # Add url to visited and update subdomains
+    VISITED.add(url)
+    subdomains(url)
+
+    # Retrieve links without fragments and complete checks to ensure validity
+    try: 
+        soup = BeautifulSoup(html, 'lxml')
+        for tag in soup.find_all('a', href=True):
+            og_url= urljoin(url, tag['href'])
+            # check validity
+            if is_valid(og_url)
+                links.add(og_url)
+    except Exception as e:
+        print(f"Error extracting links from {url}: {e}")
+    finally: # memory
+        del decoded_html
+        del soup
+        gc.collect()
     return links
 
-def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
-    try:
-        #TODO: Check for traps/infinite loops
+def is_valid(url): #done/untested
+    """ Decide whether to crawl this url (True) or not (False) """
+    global DO_NOT_ENTER
+    try:        
         parsed = urlparse(url)
+        clean_url, _ = urldefrag(url)
+        domain = parsed.netloc.lower()
+
+        if clean_url in DO_NOT_ENTER or clean_url in VISITED:
+            return False
+
         if parsed.scheme not in set(["http", "https"]):
             return False
             
-        domain = parsed.netloc.lower()
         if not (
             domain.endswith(".ics.uci.edu") or
             domain.endswith(".cs.uci.edu") or
             domain.endswith(".informatics.uci.edu") or
             domain.endswith(".stat.uci.edu")
         ):
+            return False
+            
+        if any(part in clean_url.lower() for part in trap_keywords):
+            DO_NOT_ENTER.add(clean_url)
             return False
             
         return not re.match(
@@ -81,29 +131,29 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-    except TypeError:
-        print ("TypeError for ", parsed)
-        raise
+
+    except Exception as error:
+        print(f"[IS_VALID ERROR] Failed to validate {url}: {error}")
+        DO_NOT_ENTER.add(url)
+        return False
 
 
 # HELPER FUNCTIONS:
-# 4 analytics functions
-def common_words_file(): #done/untested
-    '''Q3: Write 50 most common words and their frequency in entire set of pages crawled. '''
-    global COMMON_WORDS
-    # ignore english stop words
-    with open('Report/CommonWords.txt', 'w') as txtfile:
-        word = ''
-        for freq, word in enumerate(sorted(COMMON_WORDS.items(), key=(lamba x:x[1]), reverse=True)[:50]):
-            string += f'{freq+1}, {word[0]} - {item[1]}\n'
-        txtfile.write(word)
-    
+# 4 analytics functions + their helpers
+def unique_urls_write(): #done/untested
+    """ Q1:Writes the total # of unique URLs successfully crawled to a file """
+    with open("Report/unique_urls.txt", "w") as unique_urls:
+        unique_urls.write(f"Unique Pages: {len(VISITED)}") #Unique Pages: 1247
+    return
+
+
 def confirm_longest_page(url, pageLength): #done/untested
-    '''Check if url is the new longest page and update LONGEST_PAGE.'''
+    '''Q2: Check if url is the new longest page and update LONGEST_PAGE.'''
     global LONGEST_PAGE
     if LONGEST_PAGE[1] < pageLength:
         LONGEST_PAGE = (url, pageLength)
     return
+
 
 def longest_page_file(): #done/untested
     '''Q2: Write the longest page's url and # of words.'''
@@ -111,7 +161,19 @@ def longest_page_file(): #done/untested
     with open('LongestPage.txt', 'w') as txtfile
         txtfile.write(f'Longest Page URL: {LONGEST_PAGE[0]} - {LONGEST_PAGE[1]}')
 
-def subdomains(url):
+
+def common_words_file(): #done/untested
+    '''Q3: Write 50 most common words and their frequency in entire set of pages crawled. '''
+    global COMMON_WORDS
+    # ignore english stop words
+    with open('Report/CommonWords.txt', 'w') as txtfile:
+        word = ''
+        for freq, word in enumerate(sorted(COMMON_WORDS.items(), key=(lambda x: x[1]), reverse=True)[:50]):
+            string += f'{freq+1}, {word[0]} - {item[1]}\n'
+        txtfile.write(word)
+
+
+def subdomains(url): #done/untested
     '''Q4: Update list of subdomains (alphabetically), and # of unique pages in each.
     List contents: Subdomain, Number of Unique Pages. '''
     global SUBDOMAINS
@@ -134,19 +196,12 @@ def subdomains(url):
 
 
 def subdomain_write(): #done/untested
-    """ Writes what subdomains are visited in a file """
+    """ Q4 Writes what subdomains are visited in a file """
     with open("Report/subdomains.txt", "w") as subdomains:
         subdomains.write(f"Subdomain Number: {len(SUBDOMAINS)}\n") # Subdomain Number: 3
         subdomains.write("SubSubdomain, Number of Unique Pages Crawled in that Subdomain")
         for item in sorted(SUBDOMAINS):
             subdomains.write(f"{item}, {SUBDOMAINS[item]}") # cs.uci.edu, 25
-    return
-
-    
-def unique_urls_write(): #done/untested
-    """ Writes the total # of unique URLs successfully crawled to a file """
-    with open("Report/unique_urls.txt", "w") as unique_urls:
-        unique_urls.write(f"Unique Pages: {len(VISITED)}") #Unique Pages: 1247
     return
     
     
@@ -168,13 +223,13 @@ def tokenize(resp): #done/untested
 def word_freq(token_list): #done/untested
 #TODO: use counters instead of dict
     """ Counts word frequencies from token list """
+    global COMMON_WORDS
     for token in token_list:
         if token not in stop_words and token.isalpha():
             if token not in COMMON_WORDS:
                 COMMON_WORDS[token] = 1
             else:
                 COMMON_WORDS[token] += 1
-    return
 
 
 def word_count_check(resp): #done/untested
@@ -189,11 +244,6 @@ def word_count_check(resp): #done/untested
         return True
     
     return False # ok to crawl
-
-
-
-
-
 
 trap_keywords = [
     'ical=', 'outlook-ical', 'eventdisplay=past', 'tribe-bar-date', 'action=', 'share=', 'swiki',
